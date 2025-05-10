824a4325864cdf12f81fd51ad46c6e9405e21340
gawakawa <mashurakiryu@icloud.com>
2025-05-10 20:48:03 +0900

    :sparkles: Add sequential execution and variable definition

--- src/basic.ts
+++ src/basic.ts
@@ -1,3 +1,5 @@
+import { error, parseBasic } from 'tiny-ts-parser';
+
 export type Type =
   | { tag: 'Boolean' }
   | { tag: 'Number' }
@@ -61,13 +63,13 @@ export const typecheck = (t: Term, tyEnv: TypeEnv): Type => {
     case 'if': {
       const condTy = typecheck(t.cond, tyEnv);
       if (condTy.tag !== 'Boolean') {
-        throw 'boolean expected';
+        error('boolean expected', t.cond);
       }
 
       const thnTy = typecheck(t.thn, tyEnv);
       const elsTy = typecheck(t.els, tyEnv);
       if (!typeEq(thnTy, elsTy)) {
-        throw 'then and else have different types';
+        error('then and else have different types', t);
       }
 
       return thnTy;
@@ -79,12 +81,12 @@ export const typecheck = (t: Term, tyEnv: TypeEnv): Type => {
     case 'add': {
       const leftTy = typecheck(t.left, tyEnv);
       if (leftTy.tag !== 'Number') {
-        throw 'number expected';
+        error('number expected', t.left);
       }
 
       const rightTy = typecheck(t.right, tyEnv);
       if (rightTy.tag !== 'Number') {
-        throw 'number expected';
+        error('number expected', t.right);
       }
 
       return { tag: 'Number' };
@@ -92,7 +94,7 @@ export const typecheck = (t: Term, tyEnv: TypeEnv): Type => {
 
     case 'var': {
       if (tyEnv[t.name] === undefined) {
-        throw new Error(`unknown variable: ${t.name}`);
+        error(`unknown variable`, t);
       }
 
       return tyEnv[t.name];
@@ -111,24 +113,49 @@ export const typecheck = (t: Term, tyEnv: TypeEnv): Type => {
     case 'call': {
       const funcTy = typecheck(t.func, tyEnv);
       if (funcTy.tag !== 'Func') {
-        throw new Error('function type expected');
+        error('function type expected', t.func);
       }
 
       if (funcTy.params.length !== t.args.length) {
-        throw new Error('wrong number of arguments');
+        error('wrong number of arguments', t);
       }
 
       for (let i = 0; i < t.args.length; i++) {
         const argTy = typecheck(t.args[i], tyEnv);
         if (!typeEq(argTy, funcTy.params[i].type)) {
-          throw new Error('parameter type mismatch');
+          error('parameter type mismatch', t.args[i]);
         }
       }
 
       return funcTy.retType;
     }
 
+    case 'seq':
+      typecheck(t.body, tyEnv);
+      return typecheck(t.rest, tyEnv);
+
+    case 'const': {
+      const ty = typecheck(t.init, tyEnv);
+      const newTyEnv = { ...tyEnv, [t.name]: ty };
+      return typecheck(t.rest, newTyEnv);
+    }
+
     default:
-      throw new Error('not implemented yet');
+      throw new Error(
+        `Unknown tag: ${(t as { tag: '__invalid__' }).tag}`,
+      );
   }
 };
+
+console.log(typecheck(
+  parseBasic(`
+  const add = (x: number, y: number) => x + y;
+  const select = (b: boolean, x: number, y: number) => b ? x : y;
+
+  const x = add(1, add(2, 3));
+  const y = select(true, x, x);
+
+  y;
+`),
+  {},
+));
